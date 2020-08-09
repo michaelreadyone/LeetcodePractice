71ff9bf7966a4bebef59c805ab9885ba1a538032
mike <michael56653556@gmail.com>
2020-08-09 23:04:29 +0800

    good

--- src/Algorithms/LC0344_reverse_string.java
+++ src/Algorithms/LC0344_reverse_string.java
@@ -1,4 +1,19 @@
 package Algorithms;
 
+//Input: ["h","e","l","l","o"]
+//Output: ["o","l","l","e","h"]
+
+/**
+ * corner case: empty string, one element array
+ */
+
 public class LC0344_reverse_string {
+    public void reverseString(char[] s) {
+        char tmp;
+        for(int i = 0; i < s.length/2 ; i++){
+            tmp = s[i];
+            s[i] = s[s.length-1-i];
+            s[s.length-1-i] = tmp;
+        }
+    }
 }
