7a26530bf8c3fc8f2d74a9a009d0e75b0098d5d4
mike <michael56653556@gmail.com>
2020-09-24 18:03:18 -0400

    09_24_2020

---
+++ src/Algorithms/LC0053_maximum_subarray.java
@@ -0,0 +1,36 @@
+package Algorithms;
+
+public class LC0053_maximum_subarray {
+
+    /**
+     * Dynamic programming?
+     * @param nums
+     * @return
+     */
+//    public int maxSubArray(int[] nums) {
+//        if(nums.length == 1){
+//            return nums[0];
+//        }
+//        int ans = nums[0];
+//        for(int i = 1; i < nums.length ;i++){
+//            if(nums[i-1]>0){
+//                nums[i] += nums[i-1];
+//            }
+//            if(nums[i] > ans) {
+//                ans = nums[i];
+//            }
+//        }
+//        return ans;
+//    }
+
+    /**
+     * Divide and conquer
+     * @param nums
+     * @return
+     */
+    public int maxSubArray(int[] nums) {
+
+
+        return 0;
+    }
+}
